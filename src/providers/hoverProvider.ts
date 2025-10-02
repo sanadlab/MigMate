2f498a2d285ecab4247c72bc625c1e55d3aa9205
MatthiasKebede <msk9862@nyu.edu>
2025-10-02 04:49:54 +0400

    Update hover to fix issues

--- src/providers/hoverProvider.ts
+++ src/providers/hoverProvider.ts
@@ -9,17 +9,36 @@ export function registerHoverProvider(context: vscode.ExtensionContext) {
             {language: 'pip-requirements'}
         ],
         {
-            provideHover(document, position) {
-                const range = document.getWordRangeAtPosition(position, /[a-zA-Z0-9-_]+/);
-                const word = range ? document.getText(range) : null;
 
-                if (word) {
-                    console.log(`Hover detected on word: ${word}`);
+            // // consider doing something like
+            // { language: 'toml', pattern: '**/pyproject.toml' }
+            // provideHover(doc, pos) {
+            //     if (document.fileName.endsWith('pyproject.toml')) {return ProvideTomlHover(doc, pos)}
+            //     if (document.fileName.endsWith('requirements.txt')) {return ProvideRequirementsHover(doc, pos)}
+            //     return undefined;
+            // }
+
+            provideHover(document, position) {
+                const line = document.lineAt(position.line);
+                const lineText = line.text.trim();
+                if (lineText.startsWith('#') || lineText.length === 0) { // ignore commented/empty lines
+                    return undefined;
+                }
+
+                const wordRange = document.getWordRangeAtPosition(position, /[a-zA-Z0-9\-_]+/);
+                if (!wordRange) {return undefined;}
+                const hoveredWord = document.getText(wordRange);
+
+                const libNameMatch = lineText.match(/^([a-zA-Z0-9\-_]+)/);
+                if (libNameMatch && libNameMatch[1] === hoveredWord) {
+                    const libName = libNameMatch[1];
+                    console.log(`Hover detected on library: ${libName}`);
+
                     const markdown = new vscode.MarkdownString(
-                        `**LibMig Plugin:**\n\n[Migrate \`${word}\`](command:libmig.migrate?${JSON.stringify(word)})`
+                        `**LibMig Plugin:**\n\n[Migrate \`${libName}\`](command:libmig.migrate?${JSON.stringify(libName)})`
                     );
                     markdown.isTrusted = true;
-                    return new vscode.Hover(markdown);
+                    return new vscode.Hover(markdown, wordRange);
                 }
                 return undefined;
             }
